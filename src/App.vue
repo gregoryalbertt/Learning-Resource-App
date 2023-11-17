08f8744959f97d2599a865a59f32b4bd212ccd84
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-11-17 15:48:13 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources">{{ resource }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official guide',
+                    title: 'Official Guide',
+                    description: 'The official vue.js documentation',
+                    link: 'http://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to Google',
+                    link: 'http://google.com'
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style></style>
