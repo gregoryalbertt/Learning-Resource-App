99ecf5420b10e3fee3f7b71d653f775ddf319d9b
Gregory Albertt Santos Carvalho <gregory.carvalho@tcsindustrial.com.br>
2023-11-18 16:47:22 +0100

    Added LearningResource component

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,59 @@
+<template>
+    <BaseCard>
+        <li>
+            <div>
+                <header>
+                    <h3>{{ resource.title }}</h3>
+                    <button>Delete</button>
+                </header>
+            </div>
+            <p>{{ resource.description }}</p>
+            <nav>
+                <a :href="resource.link">View Resource</a>
+            </nav>
+        </li>
+    </BaseCard>
+</template>
+
+<script>
+import BaseCard from '../base/BaseCard.vue';
+
+export default {
+    props: {
+        resource: {},
+    },
+    components: { BaseCard }
+}
+</script>
+
+<style scoped>
+li {
+    margin: auto;
+    max-width: 40rem;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+h3 {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+}
+
+p {
+    margin: 0.5rem 0;
+}
+
+a {
+    text-decoration: none;
+    color: #ce5c00;
+}
+
+a:hover,
+a:active {
+    color: #c89300;
+}
+</style>
